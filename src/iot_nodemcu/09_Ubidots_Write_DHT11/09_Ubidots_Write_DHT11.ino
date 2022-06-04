dd630e0d64cb4a270964436cf67a8b04cb57b162
abraham <55025669+medium-technica@users.noreply.github.com>
2022-06-04 16:15:12 +0530

    Update 09_Ubidots_Write_DHT11.ino

--- src/iot_nodemcu/09_Ubidots_Write_DHT11/09_Ubidots_Write_DHT11.ino
+++ src/iot_nodemcu/09_Ubidots_Write_DHT11/09_Ubidots_Write_DHT11.ino
@@ -7,7 +7,7 @@
 #define TOKEN  "BBFF-seb79vW5XCFUmhatBKB6gmSPxoaANE"  // Put here your Ubidots TOKEN
 #define temperatureVariableID "5c6a7f521d847231bfa8444c" // Put your variable ID here
 #define humidityVariableID "5c71086a1d84723fec62b9bd" // Put your variable ID here
-#define locationVariableID "5d0dd2c21d8472636d8f4e75"
+//#define locationVariableID "5d0dd2c21d8472636d8f4e75"
 
 #define WIFISSID "Name WiFi SSID" // Put here your Wi-Fi SSID
 #define PASSWORD "password@123" // Put here your Wi-Fi password
@@ -38,15 +38,15 @@ void loop()
 
     Serial.println("Humidity: " + String(h) + "%, Temperature: " + String(t) + "°C ");
   */
-  char context[40];
-  float _lat = 123.5667;
-  float _lng = 79.0045;
-  char context_string[40] = "lat=%.2f$lng=%.2f";
+  //char context[40];
+  //float _lat = 123.5667;
+  //float _lng = 79.0045;
+  //char context_string[40] = "lat=%.2f$lng=%.2f";
   //char context_string[40] = "\"context\":{\"lat\":51.5,\"lng\":-0.11}";
-  sprintf(context, context_string, _lat, _lng);
-  //client.add(humidityVariableID, h);
-  //client.add(temperatureVariableID, t);
-  client.add("position", 10, context);
+  //sprintf(context, context_string, _lat, _lng);
+  client.add(humidityVariableID, h);
+  client.add(temperatureVariableID, t);
+  //client.add("position", 10, context);
   client.sendAll(true);
   delay(5000);
 }
