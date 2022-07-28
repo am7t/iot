53d83c6132525e9ca52d3783de10b5950900e898
abraham <55025669+medium-technica@users.noreply.github.com>
2022-07-28 18:28:25 +0530

    update

--- nodemcu/00_Demo_Ultrasonic_Range_Finder/00_Demo_Ultrasonic_Range_Finder.ino
+++ nodemcu/00_Demo_Ultrasonic_Range_Finder/00_Demo_Ultrasonic_Range_Finder.ino
@@ -4,53 +4,53 @@
 
 void setup()
 {
-  Serial.begin(115200);
-  pinMode(triggerPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+ Serial.begin(115200);
+ pinMode(triggerPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+ pinMode(ledPin, OUTPUT);
 }
 
 void loop()
 {
-  long duration;
-  int cm;
+ long duration;
+ int cm;
 
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
+ digitalWrite(triggerPin, LOW);
+ delayMicroseconds(2);
+ digitalWrite(triggerPin, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(triggerPin, LOW);
 
-  duration = pulseIn(echoPin, HIGH);
+ durationMicroSeconds = pulseIn(echoPin, HIGH);
 
-  cm = microsecondsToCentimeters(duration);
+ cm = microSecondsToCentimeters(durationMicroSeconds);
 
-  Serial.println("Distance = " + String(cm) + " cm");
+ Serial.println("Distance = " + String(cm) + " cm");
 
-  if (cm < 15)
-  {
-    digitalWrite(ledPin, LOW);
-  }
-  else
-  {
-    digitalWrite(ledPin, HIGH);
-  }
+ if (cm < 15)
+ {
+  digitalWrite(ledPin, LOW);
+ }
+ else
+ {
+  digitalWrite(ledPin, HIGH);
+ }
 
-  delay(500);
+ delay(500);
 }
 
-int microsecondsToCentimeters(long microseconds)
+int microSecondsToCentimeters(long microSeconds)
 {
-  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
-  // 340 meter per seconds -> 0.00034 meter per microseconds -> 0.034 centimeter per microseconds
+ // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+ // 340 meter per seconds -> 0.00034 meter per microseconds -> 0.034 centimeter per microseconds
 
-  // so instead of multiplying with a float value which takes relatively more time,
-  // the multiplying factor is converted to an integer by taking the reciprocal
+ // so instead of multiplying with a float value which takes relatively more computation time & memory,
+ // the multiplying factor is converted to an integer by taking the reciprocal
 
-  // 0.034 = 1 / 29.4117647059 ≈ 1 / 29
-  
-  // The ping from the sensor travels out and back, so to find the distance of the object
-  // half of the distance travelled is taken.
+ // 0.034 = 1 / 29.4117647059 ≈ 1 / 29
 
-  return microseconds / 29 / 2;
+ // The ping from the sensor travels out and back, so to find the distance of the object
+ // half of the distance travelled is taken.
+
+ return microSeconds / 29 / 2;
 }
